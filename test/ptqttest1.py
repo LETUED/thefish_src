3726aa3ead13616f9b5ea8d693681574e71fa4ec
LETUED <woo9910203626@gmail.com>
2026-07-09 20:16:54 +0900

    fix: torch를 PyQt5보다 먼저 import하도록 순서 수정 - DLL 충돌(WinError 1114) 대응

--- test/ptqttest1.py
+++ test/ptqttest1.py
@@ -1,10 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFileDialog, QFrame, QVBoxLayout
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
 import torch
 import torchvision.transforms as transforms
 from torchvision.models import resnet50
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFileDialog, QFrame, QVBoxLayout
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 import sqlite3
 from PIL import Image
 import json
